a183f02fd9bb20a3a1b5941dd90a2a407f1de351
brinth <brinth@users.noreply.github.com>
2023-10-03 09:52:27 +0000

    deploy: 42d8ba3dc85c0d347b223db15c154bc28d7dba28

--- search/all_5.js
+++ search/all_5.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['valve_5fconf_0',['valve_conf',['../structvalve__conf.html',1,'']]],
-  ['valve_5fops_1',['valve_ops',['../structvalve__ops.html',1,'']]]
+  ['wifi_5fsta_5fconf_0',['wifi_sta_conf',['../structwifi__sta__conf.html',1,'']]],
+  ['wirrigator_1',['Wirrigator',['../index.html',1,'']]]
 ];
